e279fe87057542527f427568afa5397b2f3acf1d
DuckyProject <124997286+DuckyProject@users.noreply.github.com>
2023-04-27 22:24:07 +0800

    Create temp.sh

---
+++ temp.sh
@@ -0,0 +1,37 @@
+sudo kill -9 $(sudo lsof -t -i:8088)
+
+if [ `uname -m` = "x86_64" ] ; then
+	    aarch=amd64
+elif [ `uname -m` = "aarch64" ] ; then
+	    aarch=arm64
+else
+	  echo "This aarch is not supported for the time being."
+    exit 1
+fi
+
+wget -O /DuckyClient/client https://github.com/DuckyProject/DuckyRoBot/releases/latest/download/DuckyClient-${aarch}
+
+cat >/etc/systemd/system/ducky_bot.service <<EOF
+[Unit]
+Description=ducky_bot
+Documentation=https://docs.duckybot.org
+After=network.target
+After=mysqld.service
+Wants=network.target
+
+[Service]
+WorkingDirectory=/data/ducky_bot
+ExecStart=/data/ducky_bot/client
+Restart=on-abnormal
+RestartSec=5s
+KillMode=mixed
+
+StandardOutput=null
+StandardError=syslog
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemdctl enable ducky_bot
+systemdctl restart ducky_bot

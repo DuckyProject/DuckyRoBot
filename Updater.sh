717fdf6f4cbac2719bcd9972338be20244efdcf5
DuckyProject <124997286+DuckyProject@users.noreply.github.com>
2023-05-02 20:17:59 +0800

    Create Updater.sh

---
+++ Updater.sh
@@ -0,0 +1,11 @@
+
+if [ `uname -m` = "x86_64" ] ; then
+	    aarch=amd64
+elif [ `uname -m` = "aarch64" ] ; then
+	    aarch=arm64
+else
+	  echo "This aarch is not supported for the time being."
+    exit 1
+fi
+
+wget -O /data/ducky_bot/updater https://github.com/DuckyProject/DuckyRoBot/raw/main/updater-${aarch}
